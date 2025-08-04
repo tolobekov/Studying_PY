40f4f97312df294c12d0801030af299a1f00f3b0
Tolobekov Ibrakhim <74904717+tolobekov@users.noreply.github.com>
2025-08-04 20:58:28 +0200

    Update main.py

--- Projects/TreasureIsland/main.py
+++ Projects/TreasureIsland/main.py
@@ -1,1 +1,43 @@
 
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right": ').lower()
+
+if choice1 == "left":
+    choice2 = input('You come to a lake. Do you "wait" for a boat or "swim" across? ').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at an island with three doors: one red, one yellow, and one blue. Which do you choose? ").lower()
+        if choice3 == "yellow":
+            print("🎉 You found the treasure! You win!")
+        elif choice3 == "red":
+            print("🔥 It's a room full of fire. Game over.")
+        elif choice3 == "blue":
+            print("🐻 You enter a room of beasts. Game over.")
+        else:
+            print("🚪 You chose a door that doesn't exist. Game over.")
+    else:
+        print("🐊 You got attacked by a crocodile. Game over.")
+else:
+    print("😈 You fell into a hole. Game over.")
